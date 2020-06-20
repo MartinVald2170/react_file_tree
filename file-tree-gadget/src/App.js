0234afa4cebc342292a3c9b2b94194fa39c5a1fc
MartinVald2170 <noways2170@gmail.com>
2020-06-20 15:38:46 +1000

    clicking function working

--- file-tree-gadget/src/App.js
+++ file-tree-gadget/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
   return <div> 
-  <Folder />
-  <Folder />
-  <Folder />
+  <Folder name="Desktop">
+    <Folder name="Music">
+    <File name="All_Star.mp4"/>
+    <File name="Despacito.mp4"/>
+    </Folder> 
+
+     <File name="dogs.jpg" /> 
+  <File name="cats.png" />
+
+    </Folder>
+  <Folder name="Application" />
+ 
   </div>
 }
 
-const Folder = () => {
-  const name="my_desktop";
-  return <h4>{name}</h4>
+const Folder = (props) => {
+  const [isOpen, setIsOpen ] = useState(true);
+  const { name, children } = props;
+
+  const handleClick = () => setIsOpen(!isOpen);
+  
+  return <div>
+    <span onClick={handleClick}>{name}</span>
+    <div style={{ marginLeft: "17px"}}>
+    {isOpen ? children : null}
+    </div>
+  </div>
 };
 
+const File = (props) => {
+  return <div>{props.name}</div>
+}
 
 
 
